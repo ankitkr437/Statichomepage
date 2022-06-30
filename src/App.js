a3ee93a8b5819f3950578ad4c512f8777f1da0f0
ankit kumar <ankitloharshi@gmail.com>
2022-06-30 14:11:13 +0530

    made responsive

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
 
 
             <div className='left-content-data'>
-              <h1 className='left-content-head'>
+              <p className='left-content-head'>
                 Karyashala Objectives:
-              </h1>
+              </p>
               <p className='left-content-metadata'>
                 'KARYASHALA' is a noble endeavor of Government of India by Science and Engineering Board (SERB) under Accelerate Vigyan Scheme to improve research productivity of promising PG and PhD students from universities and colleges through high-end workshops on specific themes. This program aims to provide opportunities to acquire specialized research skills. These workshops will primarily be facilitated at organizations / institutions / laboratories of national importance. PDPM Indian Institute of Information Technology, Design and Manufacturing Jamshedpur has decided to be a part of this unique initiative and share our Institutional resources with the society towards higher goals of national importance. Thus, the current workshop has come into being. This event is sponsored by SERB.
               </p>
@@ -29,18 +29,18 @@ function App() {
 
 
             <div className='left-content-data'>
-              <h1 className='left-content-head'>
+              <p className='left-content-head'>
                 About the Workshop:
-              </h1>
+              </p>
               <p className='left-content-metadata'>
                 'KARYASHALA' is aimed to provide hands-on experience to the students primarily from universities, colleges, private academic institutions, and newly established institutes in handling/troubleshooting of high-end scientific instruments and such skill development on themes required for research work. The program is meant to support motivated PG and Ph.D. level students, who are having a strong willingness to get excellence in their scientific and engineering research pursuits in the field of Bioinformatics and Computational Genomics &Proteomics. The objective of the course is to examine current issues in Mathematical Biology, Computational Biology and Bioinformatics from an engineering perspective. A major goal of this course is to introduce students studying Biology or Biochemistry to the concepts and analytical techniques of engineering. Likewise, engineers will be exposed to the terminology and concepts of Molecular Biology, Genomics and Proteomics. Students, faculty and researchers in Computer Engineering, Electrical Engineering, Bioinformatics, Biology, and Biochemistry will be the targeted participants.
               </p>
             </div>
 
             <div className='left-content-data'>
-              <h1 className='left-content-head'>
+              <p className='left-content-head'>
                 Learning Objectives:
-              </h1>
+              </p>
               <p className='left-content-metadata'>
                 <ol start="1" type="1">
                   <li>Accessing DNA &amp; Protein Data Bases </li>
@@ -57,18 +57,18 @@ function App() {
           </div>
 
           <div className='left-content-data'>
-            <h1 className='left-content-head'>
+            <p className='left-content-head'>
             Who Can Attend:
-            </h1>
+            </p>
             <p className='left-content-metadata'>
             Karyashala is open to postgraduate and PhD students from Institutes, Colleges, and Universities in India. Applicants pursuing PG/PhD programs in any discipline of Electrical Engineering, Computer Engineering, Bioinformatics, Biology, and Biochemistry are eligible. Students who have recently completed their PG programs are also eligible. Preference will be given to the students having interest in Machine Learning, Signal Processing, Mathematical Biology, Computer Science & Engineering, Computational Biology, and Bioinformatics. Candidates who have completed PhD will not be considered. Total numbers of seats are limited to 25. There is no registration fees.
             </p>
           </div>
 
           <div className='left-content-data'>
-            <h1 className='left-content-head'>
+            <p className='left-content-head'>
             Boarding, Lodging & TA:
-            </h1>
+            </p>
             <p className='left-content-metadata'>
             Boarding and lodging to the selected candidates will be borne by the organizing institute. TA will be admissible as per GoI norms.
             </p>
@@ -76,9 +76,9 @@ function App() {
 
 
           <div className='left-content-data'>
-            <h1 className='left-content-head'>
+            <p className='left-content-head'>
             About PDPM NIT Jamshedpur:
-            </h1>
+            </p>
             <p className='left-content-metadata'>
             NIT Jamshedpur is an Institute of National Importance, established by Government of India in 2005 with a focus on education and research in IT enabled Design and Manufacturing. Since its inception, PDPM NITM Jamshedpur has been playing a vital role in producing quality human resources for contribution in India’s mission of inclusive and sustainable growth. The Institute offers undergraduate, post graduate and PhD programmes in Computer Science and Engineering, Electronics and Communication Engineering, Mechanical Engineering, Design and PhD programmes in Natural Sciences (Mathematics and Physics). Under IIIT act, the Institute has been declared as an Institute of National Importance in January 2015. The Institute campus is being developed on 250 acres of land close to Dumna Airport, Jamshedpur. The Institute is locsted at a distance of 10 kms from the Jamshedpur railway station and 5.5 kms from Dumna Airport, Jamshedpur.
             </p>
@@ -169,7 +169,7 @@ function App() {
 
         </div>
       </div>
-      <div className='footer'> Copyright © 2022 nitjsr.ac.in Web Support: Aman </div>
+      <div className='footer'> Copyright © 2022 nitjsr.ac.in Web Support: Web Team</div>
     </div>
     
     </div>
